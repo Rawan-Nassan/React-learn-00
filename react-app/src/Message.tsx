9cc111f4fe156d040ad1fe4da7c87866844c1a38
Rawan-Nassan <rwan.nassan@gmail.com>
2024-06-16 20:50:47 +0200

    init0

---
+++ react-app/src/Message.tsx
@@ -0,0 +1,11 @@
+
+// PascalCasing
+function Message(){
+    //JSX: Javascript XML
+    //babeljs.io
+    const name= "Rawan";
+    return <h1>Hello {name}</h1>;
+}
+
+// To use this Component
+export default Message;
